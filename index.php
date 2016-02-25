4e7783bb02a336892df0a585199197518c5e2bee
Muki85 <mustafa.elaasad@gmail.com>
2016-02-25 13:44:03 +0100

    Made changes to initial fils
    Dodao sam sledece funkcije u klasi DB:
    
    1) funkcija __construct() i getInstance (): te dve funkcije pomazu u
    tome da nema olaksaju proces konekcije sa bazom. Tako da nema potrebe da
    se svaki put vrsi konekcija kad se trazi izvrsavanje nekog zahtev na
    jednoj stranici, nego se vrsi konekcija sa bazom jedan put i to je to,
    nema potrebe da se vrsi opet konekcija ako smo i dalje na isto stranici.
    
    2) funkcije query(), execut(), bindValue():  ove funkcije nam olaksavaju
    pisanje upite ka bazi. Forma upita je pomocu ovih funkcija moze je:
    
    $user = DB::getInstance()->query("SELECT username FROM users WHERE
    username = ?", array('Alex'));
    
    takodje funkcije vracaju GRESKA ako uslovi u upitu nisu ispunjeni.
    
    3) funkcija action(): ona nam prosto jos vise olaksava pisanje upita ka
    bazi, tako da sada upiti mogu imati sledecu formo:
    
    $user= DB::getInstance()->get('user', array ('username', '=', 'Alex'));
    
    4) funkcija get(), count(), delete (), error() : prve dve funkcija
    prikazuje korisnike u tabeli na ekranu, treca brise korisnika, cetvrta
    proverava dali ima korisnike u bazi, ako nema onda ispise "no user"
    
    5) funkcija results(): nam pomaze da izlistamo sve korisnike i ispisemo
    ih na ekranu, pomocu foreach petlje.
    
    ovde sam koristio PDO.

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
 require_once 'ooplr/core/init.php';
 
-var_dump(Config::get('mysql/host'));
+$user = DB::getInstance()->query("SELECT * FROM users");
+
+if(!$user->count()){
+	echo 'no user';
+} else {
+	foreach($user->results() as $user){
+		echo $user->username,'<br>';
+	}
+}
