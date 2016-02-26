fa1ac2b8342d0c3a03b2480003c38f64be5f7dd9
Muki85 <mustafa.elaasad@gmail.com>
2016-02-26 13:58:46 +0100

    Changes made to initial files
    Token.php i Hash.php su filovi koji su zaduzeni za bezbednost
    aplikacije.
    
    Token.php sadrzi metode koje zasticuaju aplikaciju od 'Cross-Site
    Request Forgery (CSRF)' a Hash.php  sadrzi metode koje se koriste  za
    kriptovanje unete sifre pomocu sha265 i salt.
    
    u Session.php sam dodao metodu 'flash' koja prosto pomaze da da se
    ispisuju kratke poruke dok traje secia, a kad se zavrsi, poruka se
    izbrise. Nije toliko bitna metoda ali cisto da bude to za svaki slucaj.
    
    User.php sadrzi metode koje dodaju novog korisnika u bazu podataka. a u
    file register.php sam dodao potreban kod da se izvrsi ta funkcionalnost.
    
    Ridirect.php sadrzi prostu metodu koja preusmeri korisnika a zeljenu
    stranicu. Uslacaju neke greske, aplikacija ce ga usmeriti na stranicu
    404.
    
    p.s: brisanje i dodavanje u index.php je cisto radi testiranje koda
    trenuto

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 require_once 'ooplr/core/init.php';
 
-$user = DB::getInstance()->update('users', 3, array(
-	'password' => 'newpassword',
-	'name' => 'dale garrent'
-));
+if(Session::exists('home')) {
+	echo '<p>' . Session::flash('home') . '</p>';
+}
 
--- register.php
+++ register.php
@@ -2,34 +2,56 @@
 require_once 'ooplr/core/init.php';
 
 if(Input::exists()) {
-	$validate = new Validate();
-	$validation = $validate->check($_POST, array(
-		'username' => array(
-			'required' => true,
-			'min' => 2,
-			'max' => 20,
-			'unique' => 'users'
-		),
-		'password' => array(
-			'required' => true,
-			'min' => 6
-		),
-		'password_again' => array(
-			'required' => true,
-			'matches' => 'password'
-		),
-		'name' => array(
-			'required' => true,
-			'min' => 2,
-			'max' => 50
-		)
-	));
-	
-	if($validation->passed()){
-		echo 'Passed';
-	} else {
-		foreach($validation->errors() as $error) {
-			echo $error, '<br>';
+	if(Token::check(Input::get('token'))) {
+		$validate = new Validate();
+		$validation = $validate->check($_POST, array(
+			'username' => array(
+				'required' => true,
+				'min' => 2,
+				'max' => 20,
+				'unique' => 'users'
+			),
+			'password' => array(
+				'required' => true,
+				'min' => 6
+			),
+			'password_again' => array(
+				'required' => true,
+				'matches' => 'password'
+			),
+			'name' => array(
+				'required' => true,
+				'min' => 2,
+				'max' => 50
+			)
+		));
+		
+		if($validation->passed()){
+			$user = new User();
+			
+			$salt = Hash::salt(32);
+			
+			try {
+				$user->create(array(
+					'username' => Input::get('username'),
+					'password' => Hash::make(Input::get('password'), $salt),
+					'salt' => $salt,
+					'name' => Input::get('name'),
+					'joined' => date('Y-m-d H:i:s'),
+					'group1' => 1
+					
+				));
+				
+				Session::flash('home', 'You have been registered and can now log in!');
+				Redirect::to('index.php');
+				
+			} catch(Exception $e) {
+				die($e->getMessage());
+			}
+		} else {
+			foreach($validation->errors() as $error) {
+				echo $error, '<br>';
+			}
 		}
 	}
 }
@@ -55,5 +77,8 @@ if(Input::exists()) {
 		<label for="name">Name</label>
 		<input type="text" name="name" value="<?php echo escape(Input::get('name')); ?>" id="name">
 	</div>
+	
+	<input type="hidden" name="token" value="<?php echo Token::generate();?>">
+	
 	<input type="submit" value="Register">
 </form>
